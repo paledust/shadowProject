fe24f1583ee57153c8915857cd1c594decfd9e43
Kevin <paledust@hotmail.com>
2017-01-30 21:21:25 -0500

    Prototype_1 mostly done
    Prototype_1 create a world where most object have no shape or image but only projecting shadow on the floor, player can use time control to interact with those things.

---
+++ ShadowWork/Assets/Script/Recorder.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct PlayerState
+{
+	public Vector3 position;
+	public int animState;
+	public bool direction;
+
+	public PlayerState(Vector3 position, int animState, bool direction)
+	{
+		this.position = position;
+		this.animState = animState;
+		this.direction = direction;
+	}
+}
+
+public class Recorder : MonoBehaviour {
+	[SerializeField] TestTimeController timeController;
+	[SerializeField] CarMoving carMoving;
+	Animator _animator;
+	Dictionary<int, PlayerState> state = new Dictionary<int, PlayerState>();
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponent<Animator>();
+	}
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.K))
+		{
+			isRecording = false;
+			carMoving.SetRecording(state);
+			timeController.time = 0;
+		}
+	}
+	bool isRecording = true;
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(isRecording)
+		{
+			state.Add(timeController.time, new PlayerState(transform.position, 
+															_animator.GetCurrentAnimatorStateInfo(0).shortNameHash,
+															transform.localScale.x > 0));
+		}
+	}
+}
---
+++ ShadowWork/Assets/Script/TestTimeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTimeController : MonoBehaviour {
+	public int time;
+	bool isForward = true;
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(isForward)
+		{
+			time ++;
+		}
+	}
+}
