52395783f75c85de3692844dacec78d7afa724dd
paledust <paledust@hotmail.com>
2017-02-11 02:18:34 -0500

    Solve the bug

---
+++ ShadowWork/Assets/Script/LightManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.SerializableAttribute]
+public struct LightData
+{
+	public Transform tagtTransform;
+	public int[] lightsIndex;
+}
+
+public class LightManager : MonoBehaviour {
+	public LightData[] datas; 
+	public	GameObject movePlat;
+	public float DetctRange = 1.0f;
+	public TriggerLight[] lights;
+	private bool inPosition = false;
+	// Use this for initialization
+	void Start () {
+		movePlat = GameObject.FindGameObjectWithTag("MovingPlat");
+		inPosition = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (LightData data in datas)
+		{
+			if((movePlat.transform.position - data.tagtTransform.position).magnitude <= DetctRange)
+			{
+				for(int i = 0; i < data.lightsIndex.Length; i++)
+				{
+					TurnOnLight(data.lightsIndex[i]);
+				}
+				break;
+			}
+			else
+			{
+				for(int i = 0; i < data.lightsIndex.Length; i++)
+				{
+					TurnOffLight(data.lightsIndex[i]);
+				}
+			}
+		}
+	}
+
+	void TurnOnLight(int _LightIndex)
+	{
+		if(!lights[_LightIndex].lightState())
+			lights[_LightIndex].LightOn();
+	}
+
+	void TurnOffLight(int _LightIndex)
+	{
+		if(lights[_LightIndex].lightState())
+			lights[_LightIndex].LightOff();
+	}
+}
