81c387b8cb6d8ef72516874d3ed79855a1811246
Kevin <paledust@hotmail.com>
2017-02-14 23:30:31 -0500

    Two Picture
    Scan line Flood Fill Begin

--- ShadowWork/Assets/Script/FloodFillTest/FloodFill.cs
+++ ShadowWork/Assets/Script/FloodFillTest/FloodFill.cs
@@ -17,28 +17,9 @@ public class FloodFill : MonoBehaviour {
 		count = 0;
 		mainCamera = Camera.main;
 		ifFill = false;
-		// tex2D = new Texture2D(renderTex.width,renderTex.height);
 
-		// for(int i = 0; i<= tex2D.height ; i++)
-		// {
-		// 	floodFill(0,i);
-		// }
-		floodFill(0,0);
-		//Graphics.Blit(tex2D,renderTex);
-		// tex2D.ReadPixels(new Rect(0, 0, renderTex.width, renderTex.height),0,0);
-		// tex2D.Apply();
+		floodFill(tex2D.width/2,tex2D.height/2);
 		tex2D.Apply();
-		Debug.Log(count);
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(!ifFill)
-		{
-			//floodFill(tex2D.width/2,tex2D.height/2);
-			ifFill = true;
-		}
 	}
 
 	private void floodFill(int pixel_x,int pixel_y)
@@ -51,21 +32,26 @@ public class FloodFill : MonoBehaviour {
 		// }
 		tex2D.SetPixel(pixel_x,pixel_y,targetColor);
 		
-		if(tex2D.GetPixel(pixel_x+1,pixel_y) != targetColor && pixel_x <= tex2D.width-1)
+		if(tex2D.GetPixel(pixel_x+1,pixel_y) != targetColor && pixel_x < tex2D.width)
 		{
 			floodFill(pixel_x+1,pixel_y);
 		}
-		if(tex2D.GetPixel(pixel_x,pixel_y+1) != targetColor && pixel_y <= 499)
+		if(tex2D.GetPixel(pixel_x,pixel_y+1) != targetColor && pixel_y < tex2D.width)
 		{
 			floodFill(pixel_x,pixel_y+1);
 		}
-		if(tex2D.GetPixel(pixel_x-1,pixel_y) != targetColor && pixel_x >= 4)
+		if(tex2D.GetPixel(pixel_x-1,pixel_y) != targetColor && pixel_x > 0)
 		{
 			floodFill(pixel_x-1,pixel_y);
 		}
-		if(tex2D.GetPixel(pixel_x,pixel_y-1) != targetColor && pixel_y >= 1)
+		if(tex2D.GetPixel(pixel_x,pixel_y-1) != targetColor && pixel_y > 0)
 		{
 			floodFill(pixel_x,pixel_y-1);
 		}
+	}
+
+	private void floodFill_ScanLine(int pixel_x, int pixel_y)
+	{
+		
 	}
 }
