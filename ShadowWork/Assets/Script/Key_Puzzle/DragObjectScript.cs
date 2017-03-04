5da8e688fd8765b0085b7540f041d61e071495f4
Daniel Kimsey <longlegsclassic@gmail.com>
2017-03-03 23:08:51 -0500

    Fixed Drag Script
    fixed?!

--- ShadowWork/Assets/Script/Key_Puzzle/DragObjectScript.cs
+++ ShadowWork/Assets/Script/Key_Puzzle/DragObjectScript.cs
@@ -4,12 +4,30 @@ using UnityEngine;
 
 public class DragObjectScript : MonoBehaviour {
 
-	float distance = 10;
-	public float dragSpeed = 1;
-
-		
-	void OnMouseDrag(){
-
-		transform.parent.Translate(dragSpeed,0,0);
+	bool dragEnabled = false;
+	Vector3 dragStartPosition;
+	float dragStartDistance;
+	void OnMouseDown()
+	{
+		dragEnabled = true;
+		dragStartPosition = transform.position;
+		dragStartDistance = (Camera.main.transform.position - transform.position).magnitude;
 	}
+	void Update()
+	{
+		if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1))
+		{
+			dragEnabled = false;
+		}
+	}
+	void OnMouseDrag()
+	{
+		if (dragEnabled)
+		{
+			Vector3 worldDragTo = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, dragStartDistance));
+			transform.position = new Vector3(worldDragTo.x, dragStartPosition.y, dragStartPosition.z);
+		}
+	}
+
+
 }
