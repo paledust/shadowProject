3120040bf5d597c07c6488e865c9cfcd5ef274f4
Daniel Kimsey <longlegsclassic@gmail.com>
2017-03-03 21:46:55 -0500

    Added Scene and Scripts
    Rotate script working, drag script broken

---
+++ ShadowWork/Assets/Script/Key_Puzzle/DragObjectScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObjectScript : MonoBehaviour {
+
+	float distance = 10;
+	public float dragSpeed = 1;
+
+		
+	void OnMouseDrag(){
+
+		transform.parent.Translate(dragSpeed,0,0);
+	}
+}
---
+++ ShadowWork/Assets/Script/Key_Puzzle/RotateObjectScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObjectScript : MonoBehaviour {
+
+	public float rotateSpeed = 20;
+
+	public bool dragging = false;
+
+	void OnMouseOver(){
+		if(Input.GetMouseButton(1) && dragging == false){
+			dragging = true;
+			Debug.Log ("mouseover");
+		}
+	}
+
+	void Update(){
+		if(Input.GetMouseButtonUp(1)){
+			dragging = false;
+		}
+		if(dragging == true){
+			float rotX = Input.GetAxis("Mouse X")*rotateSpeed*Mathf.Deg2Rad;
+			float rotY = Input.GetAxis("Mouse Y")*rotateSpeed*Mathf.Deg2Rad;
+
+			transform.RotateAround(transform.parent.position, Vector3.forward, -rotX);
+			transform.RotateAround(transform.parent.position, Vector3.forward, -rotY);
+		}
+
+	}
+		
+}
