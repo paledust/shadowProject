45d179bb7056dc4f10c4a0979d1b4cac1fb13eec
Daniel Kimsey <longlegsclassic@gmail.com>
2017-03-06 20:38:36 -0500

    Added Light Check for Rotation

--- ShadowWork/Assets/Script/Key_Puzzle/LightRaycastScript.cs
+++ ShadowWork/Assets/Script/Key_Puzzle/LightRaycastScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class LightRaycastScript : MonoBehaviour {
 
+	public GameObject lastRotatableObjectHit;
+
 	public float radius = 10;
 
 	// Update is called once per frame
@@ -16,7 +18,22 @@ public class LightRaycastScript : MonoBehaviour {
 
 		if(Physics.SphereCast(transform.position, radius, (forward), out hit, 100)){
 			distanceToObject = hit.distance;
-			print (distanceToObject + " " + hit.collider.gameObject.name);
+			//print (distanceToObject + " " + hit.collider.gameObject.name);
+
+			if(!lastRotatableObjectHit == null && !hit.collider.gameObject == lastRotatableObjectHit){
+				lastRotatableObjectHit.GetComponent<RotateObjectScript>().lightIsHitting = false;
+				print (lastRotatableObjectHit + " Check");
+			}
+			if(hit.collider.tag == "Rotatable"){
+				lastRotatableObjectHit = hit.collider.gameObject;
+				lastRotatableObjectHit.GetComponent<RotateObjectScript>().lightIsHitting = true;
+
+
+			} else{
+				lastRotatableObjectHit.GetComponent<RotateObjectScript>().lightIsHitting = false;
+				print ("false");
+				//lastRotatableObjectHit = null;
+			}
 		}
 	}
 }
--- ShadowWork/Assets/Script/Key_Puzzle/RotateObjectScript.cs
+++ ShadowWork/Assets/Script/Key_Puzzle/RotateObjectScript.cs
@@ -6,7 +6,8 @@ public class RotateObjectScript : MonoBehaviour {
 
 	public float rotateSpeed = 20;
 
-	bool draggable = false;
+	public bool lightIsHitting = false;
+
 	public bool dragging = false;
 
 	public bool rotateOnX = false;
@@ -28,29 +29,31 @@ public class RotateObjectScript : MonoBehaviour {
 		if(Input.GetMouseButtonUp(1)){
 			dragging = false;
 		}
-		if(dragging == true && rotateAround == true){
-			if(rotateOnX == true){
-				RotateAround(Vector3.right);
-			}
-			if(rotateOnY == true){
-				RotateAround(Vector3.up);
-			}
-			if(rotateOnZ == true){
-				RotateAround(Vector3.forward);
-			}
-		}else if(dragging == true){
-			if(rotateOnX == true){
-				Rotate(Vector3.right);
-			}
-			if(rotateOnY == true){
-				Rotate(Vector3.up);
-			}
-			if(rotateOnZ == true){
-				Rotate(Vector3.forward);
+		if(lightIsHitting == true){
+			if(dragging == true && rotateAround == true){
+				if(rotateOnX == true){
+					RotateAround(Vector3.right);
+				}
+				if(rotateOnY == true){
+					RotateAround(Vector3.up);
+				}
+				if(rotateOnZ == true){
+					RotateAround(Vector3.forward);
+				}
+			}else if(dragging == true){
+				if(rotateOnX == true){
+					Rotate(Vector3.right);
+				}
+				if(rotateOnY == true){
+					Rotate(Vector3.up);
+				}
+				if(rotateOnZ == true){
+					Rotate(Vector3.forward);
+				}
 			}
 		}
 	}
-
+		
 	void RotateAround(Vector3 axis){
 		float rotX = Input.GetAxis("Mouse X")*rotateSpeed*Mathf.Deg2Rad;
 		float rotY = Input.GetAxis("Mouse Y")*rotateSpeed*Mathf.Deg2Rad;
