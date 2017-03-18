cfb7d66c5bcb6edac1e898e7e6a0f35791cd08b2
longleggedone <longlegsclassic@gmail.com>
2017-03-18 13:09:16 -0400

    Added Lerp value script

--- ShadowWork/Assets/Script/ObjectManager/CameraMoveManager.cs
+++ ShadowWork/Assets/Script/ObjectManager/CameraMoveManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraMoveManager : MonoBehaviour {
 	public bool ifMove;
-	private float timer;
+	private float timer = 0.0f;
 	private CameraMoveInfo camMoveInfo;
 	// Use this for initialization
 	void Start () {
