e88c3fc7884e0cd92d99123279d8324de371e67f
Kevin <paledust@hotmail.com>
2017-04-25 20:29:06 -0400

    After Playtest
    I and Dan should refine the level, I will put the wall on the scene, replace the bar.

---
+++ ShadowWork/Assets/ChangeShadowColor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeShadowColor : MonoBehaviour {
+	private Color OriColor;
+	private float OriHeight;
+	// Use this for initialization
+	void Start () {
+		OriHeight = transform.position.y;
+		OriColor = GetComponent<Projector>().material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Projector>().material.color = Color.Lerp(OriColor, Color.yellow, (transform.position.y - OriHeight)/100);
+	}
+}
