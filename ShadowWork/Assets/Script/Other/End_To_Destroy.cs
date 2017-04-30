19b9de87f889235578fa30d1c8a8db1717f00fe6
Kevin <paledust@hotmail.com>
2017-04-30 15:01:31 -0400

    Change the Shader

---
+++ ShadowWork/Assets/Script/Other/End_To_Destroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End_To_Destroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Service.eventManager.Register<Kevin_Event.EndGame_Event>(End_Destroy);
+	}
+	
+	void End_Destroy(Kevin_Event.Event e){
+		StartCoroutine(KillObject());
+	}
+	IEnumerator KillObject(){
+		yield return new WaitForSeconds(1.0f);
+		Destroy(gameObject);
+	}
+}
