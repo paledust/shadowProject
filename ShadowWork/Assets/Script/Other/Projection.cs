7893102f1ef407c09cf289835eb35d2e2e47542d
Kevin <paledust@hotmail.com>
2017-05-05 00:38:39 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ ShadowWork/Assets/Script/Other/Projection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projection : MonoBehaviour {
+	[SerializeField] float Ratio;
+	private float Height;
+	private float Ori_Size;
+	private Projector project;
+	// Use this for initialization
+	void Start () {
+		Height = transform.position.y;
+		project = GetComponent<Projector>();
+		Ori_Size = project.orthographicSize;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		project.orthographicSize = Ori_Size - 1 * (transform.position.y - Height)/Ratio;
+	}
+}
