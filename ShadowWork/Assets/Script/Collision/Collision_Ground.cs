0fb819b6d15918866e46aa7b46d4a71656023c0d
longleggedone <longlegsclassic@gmail.com>
2017-05-15 23:40:57 -0400

    Changed Collision Sounds

--- ShadowWork/Assets/Script/Collision/Collision_Ground.cs
+++ ShadowWork/Assets/Script/Collision/Collision_Ground.cs
@@ -1,3 +1,15 @@
-﻿public class Collision_Ground : Collision_Base {
-    
+﻿using UnityEngine;
+using CS_Kevin;
+
+
+public class Collision_Ground : Collision_Base {
+	
+	protected override void OnTriggerEnter(Collider collider){
+		if(collider.name == "MovingBox"){
+			ON_HIT();
+			collider.GetComponent<MoveObject>().SetStatus(MOVESTATE.PULLING);
+			collider.GetComponent<MoveObject>().MoveBack();
+			Service.audioManager.PlaySound2D("BoxCollideGround", 0.8f, Random.Range(0.7f,1.0f));
+		}
+	}
 }
