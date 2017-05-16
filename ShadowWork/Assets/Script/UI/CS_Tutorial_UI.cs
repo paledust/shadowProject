30e3b54be1d4fc89e32a46aa9503d9ad83132ec6
Shelley <sc5875@nyu.edu>
2017-05-16 17:58:02 -0400

    implement Tutorial_UI Frame

---
+++ ShadowWork/Assets/Script/UI/CS_Tutorial_UI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Tutorial_UI : CS_UI_Management {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
