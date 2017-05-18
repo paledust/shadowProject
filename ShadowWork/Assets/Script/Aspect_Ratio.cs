ae10f4f37805f1f1bd41567739b88ddc9aa2527a
Kevin <paledust@hotmail.com>
2017-05-17 20:29:40 -0400

    Ratio Fixed for Real

--- ShadowWork/Assets/Script/Aspect_Ratio.cs
+++ ShadowWork/Assets/Script/Aspect_Ratio.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 
 public class Aspect_Ratio : MonoBehaviour {
 
-	[SerializeField] Vector2 myDefaultRatio = new Vector2 (16, 9);
+	[SerializeField] Vector2 myDefaultRatio = new Vector2 (16.0f, 9.0f);
 	private Vector2 myLastScreenSize = Vector2.zero;
 	private float myOrthographicSize;
+	private Vector2 viewSize;
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 		myOrthographicSize = this.GetComponent<Camera> ().orthographicSize;
 		UpdateOrthographicSize ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Screen.width != myLastScreenSize.x || Screen.height != myLastScreenSize.y) {
-			UpdateOrthographicSize ();
-		}
+		Debug.Log(Screen.width.ToString() + " " + Screen.height.ToString());
+		// if (Screen.width != myLastScreenSize.x || Screen.height != myLastScreenSize.y) {
+		// 	UpdateOrthographicSize ();
+		// }
 	}
 
 	private void UpdateOrthographicSize () {
-		this.GetComponent<Camera> ().orthographicSize = myOrthographicSize * myDefaultRatio.x / myDefaultRatio.y / (float)Screen.width * (float)Screen.height;
-		myLastScreenSize = new Vector2 (Screen.width, Screen.height);
+		float para = (myDefaultRatio.x/myDefaultRatio.y)/Screen.width * Screen.height;
+		Debug.Log(para);
+		GetComponent<Camera>().rect = new Rect(new Vector2(0, 0.5f - 0.5f/para), new Vector2(1,1f/para));
 	}
 }
