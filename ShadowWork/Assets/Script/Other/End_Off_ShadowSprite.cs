c144ad665a4f68cd98cd51f13d9ceb2db8132098
Kevin <paledust@hotmail.com>
2017-05-18 11:56:20 -0400

    New bug fixing

---
+++ ShadowWork/Assets/Script/Other/End_Off_ShadowSprite.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End_Off_ShadowSprite : MonoBehaviour {
+	[SerializeField] float _FadeTime = 1.0f;
+	SpriteRenderer sprite;
+	// Use this for initialization
+	void Start () {
+		sprite = GetComponent<SpriteRenderer>();
+		Service.eventManager.Register<Kevin_Event.EndGame_Event>(StartFade);
+	}
+	
+	// Update is called once per frame
+	void StartFade(Kevin_Event.Event e){
+		StartCoroutine(FadeOut());
+	}
+	IEnumerator FadeOut(){
+		Color tempColor = sprite.color;
+
+		for(float timer = 0.0f; timer < 1.0f; timer += Time.deltaTime/_FadeTime){
+			sprite.color = Color.Lerp(tempColor, Color.clear, timer);
+			yield return null;
+		}
+
+		yield return null;
+	}
+}
